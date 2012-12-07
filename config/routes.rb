55fc1b34dff17b07663d2ff1e7255907e09d893c
Jonny Dalgliesh <fighella@gmail.com>
2012-12-08 05:32:18 +1100

    123

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,11 @@
 CellarMasterEmails::Application.routes.draw do
-  require 'tractor'
   devise_for :users
 
   get "home/index"
 
   root :to => 'home#index'
   
-  mount Tractor::Engine, :at => '/emarketing', :as => 'tractor' 
+  mount Tractor::Engine, :at => '/emarketing', :as => 'tractor'
 ##mount Sigs::Engine => '/signatures', :as => 'sigs' 
 
 end
